2623a971671ca251ad918e469b1b21a4e768d7de
Geoffrey Brookshire <geoff.brookshire@gmail.com>
2022-03-06 11:45:40 -0800

    setup.py - version etc

--- oscina/__init__.py
+++ oscina/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from .ar_surr import ar_surr
 from .robust_est import robust_est
 from .shuff_time import lf2012, fsk2013
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from oscina.version import __version__
+
+# Load the required dependencies from the requirements file
+with open("requirements.txt") as requirements_file:
+    install_requires = requirements_file.read().splitlines()
+
 
 setup(
     name='oscina',
-    version='0.0.0',
+    version=__version__,
     description='OscInA: tools to test for oscillations in autocorrelated signals',
     url='https://github.com/gbrookshire/oscina',
     author='Geoffrey Brookshire',
-    packages=['oscina'],
-    install_requires=[
-        'tqdm',
-        'numpy>=1.18.1',
-        'sklearn',
-    ],
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    tests_require=['unittest'],
     zip_safe=False
 )
